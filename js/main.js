e6c6495b404d2dc5a4aa0dc563388037c19b86e2
VladySlavOst <vlad.ost92@gmail.com>
2016-10-05 17:03:31 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+       $("select").selecter({
+        cover: true
+    });
+ 
+  $("#owl-animal").owlCarousel({
+  	items : 1,
+  	singleItem : true,
+  	pagination : false,
+  	autoPlay : true
+  });
+
+    
+});
